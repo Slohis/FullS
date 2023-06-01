3245925bd244528cdae7a89b0053c02bded9edde
Sauli Lohis <juhk@protonmail.com>
2023-06-01 13:00:12 +0300

    Unicafe statistics in its own component

--- part01/unicafe/src/App.js
+++ part01/unicafe/src/App.js
@@ -1,5 +1,22 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+  const  {good, neutral, bad} = props
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>
+        good {good}<br />
+        neutral {neutral}<br />
+        bad {bad}<br />
+        all {good + neutral + bad}<br />
+        average {(good - bad) / (good + neutral + bad)}<br />
+        positive {good / (good + neutral + bad) * 100} %
+      </p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -12,15 +29,7 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)} >bad</button>
-      <h1>statistics</h1>
-      <p>
-        good {good}<br />
-        neutral {neutral}<br />
-        bad {bad}<br />
-        all {good + neutral + bad}<br />
-        average {(good - bad) / (good + neutral + bad)}<br />
-        positive {good / (good + neutral + bad) * 100} %
-      </p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
