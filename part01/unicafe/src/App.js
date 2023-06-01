f810c2e38c13f86f24f059a1538c137ca7339450
Sauli Lohis <juhk@protonmail.com>
2023-06-01 14:05:06 +0300

    Added StatisticLine component

--- part01/unicafe/src/App.js
+++ part01/unicafe/src/App.js
@@ -6,8 +6,17 @@ const Button = (props) => (
   </button>
 )
 
+const StatisticLine = (props) => {
+  const {text, value} = props
+  return (
+    <p>
+      {text} {value}
+  </p>
+  )
+}
+
 const Statistics = (props) => {
-  const  {good, neutral, bad} = props
+  const {good, neutral, bad} = props
   if ((good + neutral + bad) === 0) {
     return (
       <div>
@@ -17,15 +26,12 @@ const Statistics = (props) => {
   }
   return (
     <div>
-      <h1>statistics</h1>
-      <p>
-        good {good}<br />
-        neutral {neutral}<br />
-        bad {bad}<br />
-        all {good + neutral + bad}<br />
-        average {(good - bad) / (good + neutral + bad)}<br />
-        positive {good / (good + neutral + bad) * 100} %
-      </p>
+      <StatisticLine text='good' value={good} />
+      <StatisticLine text='neutral' value={neutral} />
+      <StatisticLine text='bad' value={bad} />
+      <StatisticLine text='all' value={good + neutral + bad} />
+      <StatisticLine text='average' value={(good - bad) / (good + neutral + bad)} />
+      <StatisticLine text='positive' value={(good / (good + neutral + bad)).toLocaleString(undefined, {style: 'percent'})} />
     </div>
   )
 }
@@ -42,6 +48,7 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text='good' />
       <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
       <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
