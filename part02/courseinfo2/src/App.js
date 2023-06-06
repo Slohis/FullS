2a4159b717fbe50515d5a4b03bca77bad37834bf
Sauli Lohis <juhk@protonmail.com>
2023-06-06 09:52:44 +0300

    Created course module, refactored the application to work with any amount of course parts

--- part02/courseinfo2/src/App.js
+++ part02/courseinfo2/src/App.js
@@ -1,28 +1,36 @@
-const Header = (props) => {
+const Course = ({course}) => {
   return (
     <div>
-      <h1>{props.course}</h1>
-    </div>
-  );
+      <Header heading={course.name} />
+      <Content parts={course.parts} />
+    </div> 
+  )
 }
 
-const Content = (props) => {
+const Header = ({heading}) => {
   return (
     <div>
-      <Part partInfo={props.parts[0]} />
-      <Part partInfo={props.parts[1]} />
-      <Part partInfo={props.parts[2]} />
+      <h1>{heading}</h1>
     </div>
-  );
-}
-const Part = (props) => {
-  return (
-    <div>
-      <p>{props.partInfo.name} {props.partInfo.exercises}</p>
-    </div>
-  );
+  )
 }
 
+const Content = ({parts}) => {
+  return (
+    <div>
+      {parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises} />)}
+    </div>
+  )
+}
+const Part = ({name, exercises}) => {
+  return (
+    <div>
+      <p>{name} {exercises}</p>
+    </div>
+  )
+}
+
+/*
 const Total = (props) => {
   return (
     <div>
@@ -30,36 +38,34 @@ const Total = (props) => {
                               props.parts[1].exercises +
                               props.parts[2].exercises}</p>
     </div>
-  );
+  )
 }
+*/
 
 const App = () => {
-
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
-  };
-  
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
+  }
+
+  return <Course course={course} />
 }
 
 export default App;
