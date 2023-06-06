ba8110842b835403a262047745fc9087a7eebf1c
Sauli Lohis <juhk@protonmail.com>
2023-06-06 12:54:34 +0300

    Moved Course and its subcomponents into a separate module

--- part02/courseinfo2/src/App.js
+++ part02/courseinfo2/src/App.js
@@ -1,47 +1,4 @@
-const Course = ({ name, parts }) => {
-  return (
-    <div>
-      <Header heading={name} />
-      <Content parts={parts} />
-      <Total parts={parts} />
-    </div>
-  )
-}
-
-const Header = ({ heading }) => {
-  return (
-    <div>
-      <h1>{heading}</h1>
-    </div>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises} />)}
-    </div>
-  )
-}
-const Part = ({ name, exercises }) => {
-  return (
-    <div>
-      <p>{name} {exercises}</p>
-    </div>
-  )
-}
-
-const Total = ({ parts }) => {
-  const total = parts.reduce(
-    (accu, curr) => accu + Number(curr.exercises), 0
-  )
-
-  return (
-    <div>
-      <p><b>Total of {total} exercises</b></p>
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
