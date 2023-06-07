0e80c11f26d3dc0f1c86306574e3a97a5e329155
Sauli Lohis <juhk@protonmail.com>
2023-06-07 11:59:27 +0300

    Implemented adding names to phonebook list

--- part02/phonebook/src/App.js
+++ part02/phonebook/src/App.js
@@ -1,24 +1,47 @@
 import { useState } from 'react'
 
+const ListItem = ({ id, name }) => {
+  return (
+    <li>{name}</li>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
-  ]) 
+  ])
   const [newName, setNewName] = useState('')
+
+  const addEntry = (event) => {
+    event.preventDefault()
+    console.log('submit event', event.target)
+    const newEntryObject = {
+      name: newName
+    }
+    setPersons(persons.concat(newEntryObject))
+    setNewName('')
+  }
+
+  const handleInputChange = (event) => {
+    console.log('change event', event.target.value)
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addEntry}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleInputChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map(person => <ListItem key={person.name} name={person.name} />)}
+      </ul>
     </div>
   )
 }
